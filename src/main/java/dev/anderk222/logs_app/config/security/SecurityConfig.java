46385c72c9562c09515fdc041714ef74977a1679
Anderson Macias <anderk222@gmail.com>
2025-03-30 01:28:22 -0500

    Se quita importacion no usada

--- src/main/java/dev/anderk222/logs_app/config/security/SecurityConfig.java
+++ src/main/java/dev/anderk222/logs_app/config/security/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
